23f730933a6713191631cd123ec22decc5561223
Jungwon Oh <jungwon.oh87@gmail.com>
2020-08-04 20:19:09 -0700

    todo list with vuex. project created

---
+++ src/components/Todos.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div>To Do List</div>
+    <div class="todos">
+      <div v-for="todo in allTodos" :key="todo.id" class="todo">{{todo.title}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "todos",
+  computed: mapGetters(["allTodos"]),
+};
+</script>
+
+<style scoped>
+.todos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1rem;
+}
+.todo {
+  border: 1px solid #ccc;
+  background: #41b883;
+  padding: 1rem;
+  border-radius: 5px;
+  text-align: center;
+  position: relative;
+  cursor: pointer;
+}
+</style>
